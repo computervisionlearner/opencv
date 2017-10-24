7cbfdd45f7229e4c122501bf31ce5d467d54bcda
computervisionlearner <33013384+computervisionlearner@users.noreply.github.com>
2017-10-24 22:16:17 +0800

    将logo前景照片添加到模板照片中
    用了另一种方法做对比：直接讲两个照片按一定比例相加，这样得到的图片具有一定的透明度，不是我们想要的

--- mask.py
+++ mask.py
@@ -1,8 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 20:22:43 2017
+
+@author: fs
+"""
+
 import cv2
+
 model = cv2.imread('s1.jpg')
+model1 = model.copy()
 logo = cv2.imread('test.png')
+
 rows, cols,_ = logo.shape
+
 gray_logo = cv2.cvtColor(logo,cv2.COLOR_BGR2GRAY)
+
 ret, mask = cv2.threshold(gray_logo,175,255,cv2.THRESH_BINARY) #get binary image
 mask_inv = cv2.bitwise_not(mask)                             #get inv_binary image
 
@@ -18,7 +31,10 @@ tiankeng = cv2.bitwise_and(logo,logo,mask = mask_inv)
 goal = cv2.add(wakeng,tiankeng)
 
 model[:rows,:cols] = goal
+
+model1[:rows,:cols] = cv2.add(0.5*model1[:rows,:cols],0.5*logo)
 cv2.imshow('model',model)
+cv2.imshow('model1',model1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 cv2.waitKey(1)
